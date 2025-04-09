a855f46e72b98c25f9b073ba799e085672850601
daros <daros@EsterMariani>
2025-04-09 11:35:21 -0300

    Inicio do projeto

---
+++ src/main/java/com/mycompany/exercicio7/Exercicio7.java
@@ -0,0 +1,14 @@
+package com.mycompany.exercicio7;
+
+import javax.swing.JOptionPane;
+
+public class Exercicio7 {
+
+    public static void main(String[] args) {
+
+        String nome = JOptionPane.showInputDialog("Digite um nome: ");
+
+        JOptionPane.showMessageDialog(null, "O nome digitado é: " + nome);
+
+    }
+}
